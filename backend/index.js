eaf24a8db44f6381aac219995ca470ee95854b12
kuusmaa <4j534j@gmail.com>
2024-05-22 08:57:56 +0300

    Test

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const kasutuconstmalihtsalttahanteadakassaanpushida = 2
 
 app.listen(3000, function() {
     console.log('App listening on port 3000!')
